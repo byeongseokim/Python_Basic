99abd38469205f59d9f68ae38579171cc1005838
byeongseok <kbs970318@gmail.com>
2023-07-20 00:09:55 +0900

    파이썬 터틀을 이용해 (선으로) 간단한 그림 그리기

---
+++ Python/t=turtle.Trurle()/파이썬 터틀로 그림그리기(기초).py
@@ -0,0 +1,12 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+for i in range(5):
+    t.fd(200)
+    t.lt(90)
+    t.fd(-20)
+    t.rt(90)
+    t.fd(-200)
+    t.lt(90)
+    t.fd(-20)
+    t.rt(90)
