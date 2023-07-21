72f07372f3b3f7d09f1b933231aed2d3ffed62ac
byeongseok <kbs970318@gmail.com>
2023-07-22 01:04:24 +0900

    Sort

---
+++ Python/sorted/내림차순과올림차순.py
@@ -0,0 +1,24 @@
+heroes=["아이언맨","토르","헐크", "스칼렛 위치"]
+print("정렬 전 : ", heroes)
+heroes.sort()
+print("정렬 후 : ", heroes)
+
+
+
+number=[9,6,7,1,8,4,5,3,2]
+print("정렬 전 : ", number)
+number.sort()
+print("정렬 후 : ", number)
+
+
+
+number=[9,6,7,1,8,4,5,3,2]
+new_list = sorted(number)
+print("number : ", number)
+print("new_list : ", new_list)
+
+
+number=[9,6,7,1,8,4,5,3,2]
+new_list = sorted(number, reverse=True)  #내림차순, 역순 
+print("number : ", number)
+print("new_list : ", new_list)
