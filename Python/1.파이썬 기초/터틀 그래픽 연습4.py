9b34144031fdc0fe74c01be95cf63e1d8c5cd028
byeongseok <kbs970318@gmail.com>
2023-07-30 22:36:28 +0900

    터틀 그래픽 연습

---
+++ Python/1.파이썬 기초/터틀 그래픽 연습4.py
@@ -0,0 +1,13 @@
+# 직접 작성 함
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
