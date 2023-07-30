e8c6a6cc97dec896025c696910ddbc02adfb318a
byeongseok <kbs970318@gmail.com>
2023-07-30 22:37:31 +0900

    오륜기 (혼자 안보고 작성)

---
+++ Python/1.파이썬 기초/오륜기 연습 9.py
@@ -0,0 +1,27 @@
+import turtle
+t=turtle.Turtle()
+t.shape('turtle')
+t.goto(0,0)
+t.circle(100)
+t.up()
+
+t.goto(180,0)
+t.down()
+t.circle(100)
+t.up()
+
+t.goto(360,0)
+t.down()
+t.circle(100)
+t.up()
+
+t.goto(90,-160)
+t.down()
+t.circle(100)
+t.up()
+
+t.goto(270,-160)
+t.down()
+t.circle(100)
+
+#좌표 설정 하는게 어려웠다..
