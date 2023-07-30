8219ab77dcdad670c84b728aa2389661aeb473b9
byeongseok <kbs970318@gmail.com>
2023-07-30 21:47:44 +0900

    터틀 그래픽 원과 다각형

---
+++ Python/t=turtle.Trurle()/그림 그리기/터틀 그래픽으로 원과 다각형 그리기.py
@@ -0,0 +1,17 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle") #거북이 모양의 붓(?)을 보여줌
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+
+t.circle(100) #반지름 100 설정하고 원그림
