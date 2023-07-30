1472ea15f536f8ec010b692820d52fb6a2805b8e
byeongseok <kbs970318@gmail.com>
2023-07-30 21:47:53 +0900

    삼각형 그려보기

---
+++ Python/t=turtle.Trurle()/그림 그리기/터틀그래픽으로 삼각형 그려보기.py
@@ -0,0 +1,21 @@
+#아래 코드는 내가 혼자 해본것
+
+import turtle #터틀 모듈 사용하기 위해
+t = turtle.Turtle() #거북이 생성
+t.forward(100) #100만큼 앞으로 이동
+t.left(125) #왼쪽125도
+t.forward(75) #75만큼 이동
+t.left(100) #왼쪽 100도
+t.forward(85) #85만큼 이동
+
+
+# 소스 코드 
+import turtle 
+t=turtle.Turtle()
+t.shape("turtle")
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+
